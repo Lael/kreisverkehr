7dd8b4fdcf2b719f9090f6cb3c272da26b27ac38
Lael Costa <Lael_Costa@Brown.edu>
2019-02-21 21:00:39 -0500

    Creates PNGs and maybe also AVIs?

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,4 +1,9 @@
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Config {
     private int width;
@@ -6,6 +11,13 @@ public class Config {
 
     private double totalLength;
     private double framesPerSecond;
+
+    private Config(int width, int height, double totalLength, double framesPerSecond) {
+        this.width = width;
+        this.height = height;
+        this.totalLength = totalLength;
+        this.framesPerSecond = framesPerSecond;
+    }
 
     public int getWidth() {
         return width;
@@ -23,7 +35,26 @@ public class Config {
         return framesPerSecond;
     }
 
-    public static Config parseFromFile(File configFile) {
-        return new Config();
+    public static Config parseFromFilePath(String configFilePath) {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(configFilePath));
+            Map<String, String> values = new HashMap<>();
+            for (String line : lines) {
+                String[] parts = line.split(": ");
+                if (parts.length != 2) {
+                    System.err.println("Malformed config line: " + line);
+                } else {
+                    values.put(parts[0], parts[1]);
+                }
+            }
+            return new Config(
+                    Integer.parseInt(values.get("width")),
+                    Integer.parseInt(values.get("height")),
+                    Double.parseDouble(values.get("length")),
+                    Double.parseDouble(values.get("fps")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Config(800, 600, 3.0, 60);
     }
 }
--- src/main/java/KreisverkehrRunner.java
+++ src/main/java/KreisverkehrRunner.java
@@ -10,18 +10,19 @@ public class KreisverkehrRunner {
         System.out.println("config: " + configFilePath);
         System.out.println("output: " + outputFilePath);
 
-        File configFile = new File(configFilePath);
-        // do stuff
+        Config config = Config.parseFromFilePath(configFilePath);
 
-        Config config = Config.parseFromFile(configFile);
+        // change to change the result
         Scene scene = new Scene(config);
 
         // get number of frames
         int numFrames = (int) Math.ceil(config.getFramesPerSecond() * config.getTotalLength());
         double frameTime = 1.0 / config.getFramesPerSecond();
-        int numZeros = (int) (Math.log(numFrames + 1) / Math.log(10)) - 1;
+        int numZeros = (int) (Math.log(numFrames + 1) / Math.log(10));
 
         File pngsDir = new File(".pngs");
+
+        deleteDir(pngsDir);
 
         if (!pngsDir.mkdirs()) {
             System.err.println("Failed to create .pngs/");
@@ -30,10 +31,11 @@ public class KreisverkehrRunner {
 
         for (int i = 0; i < numFrames; i++) {
             BufferedImage image = scene.getFrame(frameTime * i);
-            String filename = String.format(".pngs/frame%0" + numZeros + "d", i + 1);
+            String filename = String.format(".pngs/frame%0" + numZeros + "d.png", i + 1);
             File frameFile = new File(filename);
             try {
                 ImageIO.write(image, "png", frameFile);
+                System.out.println("writing " + filename);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -41,15 +43,37 @@ public class KreisverkehrRunner {
 
         try {
             Runtime runtime = Runtime.getRuntime();
-            Process ffmpegProcess = runtime.exec(
-                    String.format("ffmpeg -framerate %f -pattern_type glob -i '.pngs/*.png' -c:v ffv1 %s",
-                            config.getFramesPerSecond(),
-                            outputFilePath));
+            String command = String.format("ffmpeg -framerate %f -pattern_type glob -i '/Users/laelcosta/Desktop/kreisverkehr/.pngs/*.png' -c:v ffv1 %s",
+                    config.getFramesPerSecond(),
+                    outputFilePath);
+            System.out.println(command);
+            Process ffmpegProcess = runtime.exec(command);
             ffmpegProcess.waitFor();
+            deleteDir(pngsDir);
             File outputFile = new File(outputFilePath);
             Desktop.getDesktop().open(outputFile);
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void deleteDir(File dir) {
+        File[] files = dir.listFiles();
+        if (dir.exists() && files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    deleteDir(file);
+                } else {
+                    file.delete();
+                }
+            }
+            dir.delete();
+        }
+    }
+
+    private static void deleteFile(File file) {
+        if (!file.delete()) {
+            System.err.println("Failed to delete file: " + file.getName());
         }
     }
 }
